27c15e8a41e0f92b2884c95a5261cf881d6afb5a
kronangle <akron@clearanglestudios.co.uk>
2025-06-24 11:02:09 +0100

    Lab 2 readability

--- 4.1 - Unit testing/src/test/java/com/agile/calculator/CalculatorTest.java
+++ 4.1 - Unit testing/src/test/java/com/agile/calculator/CalculatorTest.java
@@ -1,82 +1,166 @@
+//package com.agile.calculator;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//import junit.framework.Assert;
+//
+//public class CalculatorTest {
+//	
+//	
+//
+//	@Test
+//	public void subtractionTest(){
+////		Setup
+//		Calculator calc = new Calculator();
+//		String testNumbers = "10,7";
+//		
+//
+////		Act
+//		int result = calc.subtract(testNumbers);
+//				
+////		Assert
+//		assertEquals(3, result);
+//				
+//	}
+//	
+//	@Test
+//	public void multiplyTest(){
+////		Setup
+//		Calculator calc = new Calculator();
+//		String testNumbers = "10,7";
+//		
+//		
+////		Act
+//		int result = calc.multiply(testNumbers);
+//		
+////		Assert
+//		assertEquals(70, result);
+//		
+//	}
+//	
+//	@Test
+//	public void divideTest(){
+////		Setup
+//		Calculator calc = new Calculator();
+//		String testNumbers = "21,7";
+//		
+//		
+////		Act
+//		int result = calc.divide(testNumbers);
+//		
+////		Assert
+//		assertEquals(3, result);
+//		
+//	}
+//	
+//	@Test(expected = ArithmeticException.class)
+//	public void divideByZeroTest(){
+////		Setup
+//		Calculator calc = new Calculator();
+//		String testNumbers = "21,0";
+//		
+//		
+////		Act
+//		int result = calc.divide(testNumbers);
+//	}
+//	
+//	@Test
+//	public void subtractionWithLetterTest(){
+////		Setup
+//		Calculator calc = new Calculator();
+//		String testNumbers = "20,a";
+//		
+//		
+////		Act
+//		int result = calc.subtract(testNumbers);
+//		
+////		Assert
+//		assertEquals(-9999, result);
+//	}
+//}
+
 package com.agile.calculator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import junit.framework.Assert;
+import com.agile.calculator.Calculator;
 
 public class CalculatorTest {
+
+	private Calculator calculator;
 	
+	@Before
+	public void setUp() {
+		calculator = new Calculator();
+	}
+	
+	@After
+	public void tearDown() {
+		calculator = null;
+	}
 	
 
 	@Test
-	public void subtractionTest(){
-//		Setup
-		Calculator calc = new Calculator();
-		String testNumbers = "10,7";
-		
+	public void subtractTest() {
+		int expected = 2;
+		int actual = calculator.subtract("5,3");
+		assertEquals(expected, actual);
+	}
 
-//		Act
-		int result = calc.subtract(testNumbers);
-				
-//		Assert
-		assertEquals(3, result);
-				
-	}
-	
 	@Test
-	public void multiplyTest(){
-//		Setup
-		Calculator calc = new Calculator();
-		String testNumbers = "10,7";
-		
-		
-//		Act
-		int result = calc.multiply(testNumbers);
-		
-//		Assert
-		assertEquals(70, result);
-		
+	public void subtractByNegative() {
+		int expected = -8;
+		int actual = calculator.subtract("-5,3");
+		assertEquals(expected, actual);
 	}
-	
+
 	@Test
-	public void divideTest(){
-//		Setup
-		Calculator calc = new Calculator();
-		String testNumbers = "21,7";
-		
-		
-//		Act
-		int result = calc.divide(testNumbers);
-		
-//		Assert
-		assertEquals(3, result);
-		
+	public void subtractByMiddleNumbers() {
+		int expected = 2;
+		int actual = calculator.subtract("4,2");
+		assertEquals(expected, actual);
 	}
-	
-	@Test(expected = ArithmeticException.class)
-	public void divideByZeroTest(){
-//		Setup
-		Calculator calc = new Calculator();
-		String testNumbers = "21,0";
-		
-		
-//		Act
-		int result = calc.divide(testNumbers);
-	}
-	
+
 	@Test
-	public void subtractionWithLetterTest(){
-//		Setup
-		Calculator calc = new Calculator();
-		String testNumbers = "20,a";
-		
+	public void divide() {
+		int expected = 5;
+		int actual = calculator.divide("10,2");
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void divideByNegative() {
+		int expected = -5;
+		int actual = calculator.divide("10,-2");
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void multiplySmallNumber() {
+		int expected = 50;
+		int actual = calculator.multiply("25,2");
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void multiplyBigNumber() {
+//		Arrange
+		int expected = 600;
 		
 //		Act
-		int result = calc.subtract(testNumbers);
+		int actual = calculator.multiply("150,4");
 		
 //		Assert
-		assertEquals(-9999, result);
+		assertEquals(expected, actual);
 	}
+	
+	@Test(expected = java.lang.ArithmeticException.class)
+	public void dividewithsomeexception() {
+		calculator.divide("10,0");
+	}
+
 }
+
